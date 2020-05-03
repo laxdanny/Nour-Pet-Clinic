fbbefc6a46c7fa67166c845f734cf4c44b3b3dcc
laxdanny <aboodi.nour@gmail.com>
2020-05-03 13:57:37 +0400

    add gitignore

---
+++ src/main/java/nour/springframework/nourpetclinic/NourPetClinicApplication.java
@@ -0,0 +1,13 @@
+package nour.springframework.nourpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NourPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NourPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nour/springframework/nourpetclinic/NourPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package nour.springframework.nourpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NourPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
